d92f29622ff1ed4a2e7746a11dac3e4ad97510b6
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-18 17:43:19 +0530

    Create and style Product Loading component

---
+++ src/components/ProductLoading.jsx
@@ -0,0 +1,29 @@
+const ProductLoading = () => {
+  const skeletonCards = [];
+  for (let i = 0; i < 12; i++) {
+    skeletonCards.push(
+      <div className="h-[31rem] flex flex-col justify-between gap-1 outline outline-1 outline-neutral-200 bg-slate-50  rounded-sm ">
+        <div className="animate-pulse h-5/6 bg-neutral-300"></div>
+        <div className="flex flex-col justify-between p-2">
+          <div className="animate-pulse h-4 w-5/6 mt-2 bg-neutral-300 rounded"></div>
+          <div className="animate-pulse h-4 w-1/5 mt-2 bg-neutral-300 rounded"></div>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+        gap: "10px",
+      }}
+      className="p-1"
+    >
+      {skeletonCards}
+    </div>
+  );
+};
+
+export default ProductLoading;
