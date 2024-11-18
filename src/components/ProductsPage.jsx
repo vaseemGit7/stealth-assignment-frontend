f70ddd2ce11db799389e1b1e1321b72a75815bfa
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-18 14:51:28 +0530

    Update code to render product cards

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import getProducts from "../api/API.js";
 import { useState } from "react";
+import ProductCard from "./ProductCard.jsx";
 
 const ProductsPage = () => {
   const [products, setProducts] = useState(null);
@@ -16,9 +17,27 @@ const ProductsPage = () => {
   console.log("Products: ", products);
 
   return (
-    <>
-      <button onClick={fetchProducts}>Fetch Products</button>
-    </>
+    <div className="max-w-[90rem]  mx-auto ">
+      <button
+        className="bg-slate-800 p-2 rounded-md my-2 text-neutral-50"
+        onClick={fetchProducts}
+      >
+        Fetch Products
+      </button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
+          gap: "10px",
+        }}
+        className="p-1"
+      >
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.code} product={product} />
+          ))}
+      </div>
+    </div>
   );
 };
 
