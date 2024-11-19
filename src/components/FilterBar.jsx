d369a0c54f8a30f12df756a6cd3604ffed31c17b
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-19 21:48:43 +0530

    Update Filter bar UI

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
 import FilterOptions from "./FilterOptions";
 
@@ -6,10 +6,27 @@ const FilterBar = ({ filterBarToggle, handleFilterBar }) => {
   const resultData = useSelector((state) => state.dataReducer);
   const dialogModal = useRef();
 
-  console.log(resultData);
+  const [toggleFacet, setToggleFacet] = useState({
+    Category: false,
+    Brand: false,
+    Rating: false,
+    Size: false,
+    Color: false,
+  });
 
   const getFacet = (facetCode) => {
     return resultData?.facets?.find((item) => item.code === facetCode);
+  };
+
+  const handleFacetToggle = (section) => {
+    setToggleFacet((prevState) => ({
+      ...prevState,
+      [section]: !prevState[section],
+    }));
+  };
+
+  const shouldShowFacet = (facet) => {
+    return facet?.values?.some((value) => value.count !== 0);
   };
 
   const categoriesFacet = getFacet("categories");
@@ -27,26 +44,59 @@ const FilterBar = ({ filterBarToggle, handleFilterBar }) => {
   }, [filterBarToggle]);
 
   return (
-    <>
+    <div>
       {filterBarToggle && (
         <dialog
           ref={dialogModal}
-          className="fixed left-3/4 border-none p-3 rounded-lg"
+          className="left-[79%] h-full w-96 border-none outline outline-1 outline-neutral-200 rounded p-3"
         >
-          <button
-            className="outline outline-1 outline-neutral-900 p-1"
-            onClick={handleFilterBar}
-          >
-            Close
-          </button>
-          <FilterOptions facetName={"Category"} facet={categoriesFacet} />
-          <FilterOptions facetName={"Brand"} facet={brandsFacet} />
-          <FilterOptions facetName={"Rating"} facet={ratingsFacet} />
-          <FilterOptions facetName={"Size"} facet={sizesFacet} />
-          <FilterOptions facetName={"Color"} facet={colorsFacet} />
+          <div className="flex justify-between mb-5">
+            <p className="text-2xl text-neutral-800 font-semibold">Filter</p>
+            <button onClick={handleFilterBar}>X</button>
+          </div>
+          {shouldShowFacet(categoriesFacet) && (
+            <FilterOptions
+              facetName={"Category"}
+              facet={categoriesFacet}
+              handleFacetToggle={handleFacetToggle}
+              toggleFacet={toggleFacet}
+            />
+          )}
+          {shouldShowFacet(brandsFacet) && (
+            <FilterOptions
+              facetName={"Brand"}
+              facet={brandsFacet}
+              handleFacetToggle={handleFacetToggle}
+              toggleFacet={toggleFacet}
+            />
+          )}
+          {shouldShowFacet(ratingsFacet) && (
+            <FilterOptions
+              facetName={"Rating"}
+              facet={ratingsFacet}
+              handleFacetToggle={handleFacetToggle}
+              toggleFacet={toggleFacet}
+            />
+          )}
+          {shouldShowFacet(sizesFacet) && (
+            <FilterOptions
+              facetName={"Size"}
+              facet={sizesFacet}
+              handleFacetToggle={handleFacetToggle}
+              toggleFacet={toggleFacet}
+            />
+          )}
+          {shouldShowFacet(colorsFacet) && (
+            <FilterOptions
+              facetName={"Color"}
+              facet={colorsFacet}
+              handleFacetToggle={handleFacetToggle}
+              toggleFacet={toggleFacet}
+            />
+          )}
         </dialog>
       )}
-    </>
+    </div>
   );
 };
 
--- src/components/FilterOptions.jsx
+++ src/components/FilterOptions.jsx
@@ -1,7 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { setParam } from "../actions/filterActions";
 
-const FilterOptions = ({ facetName, facet }) => {
+const FilterOptions = ({
+  facetName,
+  facet,
+  handleFacetToggle,
+  toggleFacet,
+}) => {
   const paramsData = useSelector((state) => state.paramsReducer);
   const dispatch = useDispatch();
 
@@ -17,20 +22,26 @@ const FilterOptions = ({ facetName, facet }) => {
   console.log(paramsData);
 
   return (
-    <div className="flex flex-col gap-3">
-      <p className="font-medium text-base">{facetName}</p>
+    <div className="flex flex-col">
+      <div
+        className="flex mb-2 justify-between items-center cursor-pointer"
+        onClick={() => handleFacetToggle(facetName)}
+      >
+        <p className="font-medium text-lg">{facetName}</p>
+      </div>
       {facet &&
+        toggleFacet[facetName] &&
         facet.values.map(
           (option) =>
             option.count !== 0 && (
               <div
                 key={option.code}
-                className="flex justify-between items-center gap-3"
+                className="flex justify-between items-center gap-3 mb-3"
               >
                 <label className="flex items-center gap-2 text-base font-normal cursor-pointer">
                   <input
                     type="checkbox"
-                    className="h-4 w-4 rounded-sm accent-neutral-800"
+                    className="h-5 w-5 rounded-sm accent-neutral-800"
                     value={option.code}
                     checked={
                       Array.isArray(paramsData[facet.code]) &&
