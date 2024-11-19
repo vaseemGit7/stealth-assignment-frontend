d89353b0ac098281ae6f0771f6da5dbf020ac9d8
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-19 15:38:45 +0530

    Update store with paramsReducer

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,11 @@
 import { combineReducers, createStore } from "redux";
 import dataReducer from "./reducers/dataReducer";
+import paramsReducer from "./reducers/paramsReducer";
 
-const combinedReducer = combineReducers({ dataReducer: dataReducer });
+const combinedReducer = combineReducers({
+  dataReducer: dataReducer,
+  paramsReducer: paramsReducer,
+});
 
 const store = createStore(combinedReducer);
 
