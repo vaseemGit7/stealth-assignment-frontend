7d8b978c508e657e418cee669e6026e8fdfce398
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-20 00:03:29 +0530

    Update code

--- src/components/ProductLoading.jsx
+++ src/components/ProductLoading.jsx
@@ -2,7 +2,10 @@ const ProductLoading = () => {
   const skeletonCards = [];
   for (let i = 0; i < 12; i++) {
     skeletonCards.push(
-      <div className="h-[31rem] flex flex-col justify-between gap-1 outline outline-1 outline-neutral-200 bg-slate-50  rounded-sm ">
+      <div
+        key={i}
+        className="h-[31rem] flex flex-col justify-between gap-1 outline outline-1 outline-neutral-200 bg-slate-50  rounded-sm "
+      >
         <div className="animate-pulse h-5/6 bg-neutral-300"></div>
         <div className="flex flex-col justify-between p-2">
           <div className="animate-pulse h-4 w-5/6 mt-2 bg-neutral-300 rounded"></div>
