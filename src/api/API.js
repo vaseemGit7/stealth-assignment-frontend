e9f478ff29f7a94a667eae4ea9311725665e11e5
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-11-19 17:44:19 +0530

    Update code to handle additional param queries

--- src/api/API.js
+++ src/api/API.js
@@ -2,17 +2,51 @@ import axios from "axios";
 
 const productsApi = axios.create({ baseURL: "http://localhost:5000" });
 
+const getOptionalParams = (paramsData) => {
+  let optionalParams = "";
+  let isFirstParam = true;
+
+  Object.entries(paramsData).forEach(([key, values]) => {
+    if (Array.isArray(values) && values.length > 0) {
+      values.forEach((value) => {
+        if (isFirstParam) {
+          optionalParams += `?${key}=${value}`;
+          isFirstParam = false;
+        } else {
+          optionalParams += `&${key}=${value}`;
+        }
+      });
+    } else if (
+      !Array.isArray(values) &&
+      values !== undefined &&
+      values !== null &&
+      values !== ""
+    ) {
+      if (isFirstParam) {
+        optionalParams += `?${key}=${values}`;
+        isFirstParam = false;
+      } else {
+        optionalParams += `&${key}=${values}`;
+      }
+    }
+  });
+
+  return optionalParams;
+};
+
 const getProducts = (pageNumber, pageSize, paramsState) => {
+  const optionalParams = getOptionalParams(paramsState);
+
   const params = {
     pageNumber: pageNumber,
     pageSize: pageSize,
-    categories: paramsState?.categories?.[0],
   };
 
+  //Intended to simulate network delay
   return new Promise((resolve) => {
     setTimeout(() => {
       productsApi
-        .get("/products", { params })
+        .get(`/products${optionalParams}`, { params })
         .then((res) => {
           console.log(params);
           resolve(res.data);
@@ -21,7 +55,7 @@ const getProducts = (pageNumber, pageSize, paramsState) => {
           console.log(err);
           resolve(null);
         });
-    }, 2000);
+    }, 1500);
   });
 };
 
